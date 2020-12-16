3376059c61d4e0f46ed8babb00e903d24c2feede
asirasani <aswini.sirasani@gmail.com>
2020-12-16 21:48:52 +0530

    Jest TC
    Unit TC

---
+++ tests/unit/Banner.spec.js
@@ -0,0 +1,11 @@
+
+
+import { shallowMount, mount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+import Banner from '@/components/Banner.vue'
+
+describe('Banner.vue', () => {
+  test("Banner Component test", () => {
+    expect(true).toBe(true);
+  });
+})
---
+++ tests/unit/Home.spec.js
@@ -0,0 +1,10 @@
+
+
+import { shallowMount, mount } from '@vue/test-utils'
+import Home from '@/views/Home.vue'
+
+describe('Home.vue', () => {
+  test("Home Component test", () => {
+    expect(true).toBe(true);
+  });
+})
